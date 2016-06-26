6e9b3dccba4d30f68d297d01a797168596480cda
Phyoe Khant <pisces.pkz@gmail.com>
2016-06-26 14:11:28 -0700

    Add Action bar and Action items

--- app/src/main/java/xyz/phyoekhant/harthapadaythar/activities/MainActivity.java
+++ app/src/main/java/xyz/phyoekhant/harthapadaythar/activities/MainActivity.java
@@ -1,13 +1,18 @@
 package xyz.phyoekhant.harthapadaythar.activities;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import xyz.phyoekhant.harthapadaythar.R;
@@ -17,6 +22,7 @@ import xyz.phyoekhant.harthapadaythar.utils.JokeTellerConstants;
 public class MainActivity extends AppCompatActivity {
 
     private FrameLayout flContainer;
+    private TextView tvSearchGuide;
     private Button btnPreviousJoke;
     private Button btnNextJoke;
 
@@ -29,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        tvSearchGuide = (TextView) findViewById(R.id.tv_search_jokes);
         flContainer = (FrameLayout) findViewById(R.id.fl_container);
 
         if (savedInstanceState == null) {
@@ -78,7 +85,28 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+
+        MenuItem searchMenuItem = menu.findItem(R.id.action_search);
+        MenuItemCompat.setOnActionExpandListener(searchMenuItem, new MenuItemCompat.OnActionExpandListener() {
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem item) {
+                tvSearchGuide.setVisibility(View.VISIBLE);
+                flContainer.setVisibility(View.INVISIBLE);
+                btnNextJoke.setVisibility(View.INVISIBLE);
+                btnPreviousJoke.setVisibility(View.INVISIBLE);
+                return true;
+            }
+
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem item) {
+                tvSearchGuide.setVisibility(View.INVISIBLE);
+                flContainer.setVisibility(View.VISIBLE);
+                btnNextJoke.setVisibility(View.VISIBLE);
+                btnPreviousJoke.setVisibility(View.VISIBLE);
+                return true;
+            }
+        });
         return true;
     }
 
@@ -89,9 +117,12 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(id) {
+            case R.id.action_settings:
+                return true;
+            case R.id.action_love:
+                Toast.makeText(getApplicationContext(), getString(R.string.lbl_love_the_joke), Toast.LENGTH_SHORT).show();
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
